2a6cc4609f4f2d2a61faca4db828ca3ec83648b0
PutraXz <ytprem200100@gmail.com>
2022-05-31 11:12:45 +0700

    better

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,57 +1,57 @@
 <footer class="bg-light text-dark ">
-  <div class="container text-start text-md-left">
-      <div class="row text-start text-md-left">
-          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-              <h5 class="text-uppercase mb-4 font-weight-bold text-dark">Perusahaan</h5>
-              <p class="">Perusahaan</p>
-              <p class="">Produk</p>
-              <p class="">Blog</p>
-          </div>
+    <div class="container text-start text-md-left">
+        <div class="row text-start text-md-left">
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-dark">Perusahaan</h5>
+                <p class="">Perusahaan</p>
+                <p class="">Produk</p>
+                <p class="">Blog</p>
+            </div>
 
-          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-              <h5 class="text-uppercase mb-4 font-weight-bold text-dark">Karir</h5>
-              <p class="">Magang</p>
-              <p class="">Profesional</p>
-          </div>
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-dark">Karir</h5>
+                <p class="">Magang</p>
+                <p class="">Profesional</p>
+            </div>
 
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-              <h5 class="text-uppercase mb-4 font-weight-bold text-dark">Hubungi Kami</h5>
-              <p class="">Bantuan</p>
-              <p class="">Kontak</p>
-          </div>
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold text-dark">Hubungi Kami</h5>
+                <p class="">Bantuan</p>
+                <p class="">Kontak</p>
+            </div>
 
-          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto ">
-              <img src="{{ ('asset/img/logo-footer.png') }}"  class=" bg-light" style="width: 192px; height: 68px;">
-              <h5 class="text-uppercase mb-4 mt-5 ms-md-2 font-weight-bold text-dark">Unduh Aplikasi</h5>
-          </div>
-      </div>
-      <div class="row text-start text-md-left ">
-          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-              <p class="">Kebijakan Privasi</p>
-              <p class="">Syarat dan Ketentuan</p>
-          </div>
-          <div class="col-md-8 col-lg-8 col-xl-8 mx-auto mt-3">
-              <p class="">Pengaturan Cookie</p>
-          </div>
-      </div>
-      <hr class="">
-      <div class="row text-start text-md-left">
-          <div class="col-md-9 col-lg-9 col-5 mx-auto mt-3">
-              <p class="text-muted">2022 Nino | Nino adalah merek milik dari PT. Mega Inovasi Jayatama.</p>
-          </div>
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto ">
+                <img src="{{ ('asset/img/logo-footer.png') }}"  class=" bg-light" style="width: 192px; height: 68px;">
+                <h5 class="text-uppercase mb-4 mt-5 ms-md-2 font-weight-bold text-dark">Unduh Aplikasi</h5>
+            </div>
+        </div>
+        <div class="row text-start text-md-left ">
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                <p class="">Kebijakan Privasi</p>
+                <p class="">Syarat dan Ketentuan</p>
+            </div>
+            <div class="col-md-8 col-lg-8 col-xl-8 mx-auto mt-3">
+                <p class="">Pengaturan Cookie</p>
+            </div>
+        </div>
+        <hr class="">
+        <div class="row text-start text-md-left">
+            <div class="col-md-9 col-lg-9 col-5 mx-auto mt-3">
+                <p class="text-muted">2022 Nino | Nino adalah merek milik dari PT. Mega Inovasi Jayatama.</p>
+            </div>
 
-          <div class="col-md-2 col-lg-2 col-7 mx-auto mt-3">
-              <div class="text-end text-md-right">
-                  <ul class="list-unstyled list-inline">
-                      <li class="list-inline-item">
-                          <a href="#" class="btn-floating text-muted" style="font-size: 23px !important"><i class='bx bxl-instagram' ></i></a>
-                          <a href="#" class="btn-floating text-muted" style="font-size: 23px !important"><i class='bx bxl-youtube' ></i></a>
-                          <a href="#" class="btn-floating text-muted" style="font-size: 23px !important"><i class='bx bxl-facebook-circle'></i></a>
-                          <a href="#" class="btn-floating text-muted" style="font-size: 23px !important"><i class='bx bxl-twitter' ></i></a>
-                      </li>
-                  </ul>
-              </div>
-          </div>
-      </div>
-  </div>
+            <div class="col-md-2 col-lg-2 col-7 mx-auto mt-3">
+                <div class="text-end text-md-right">
+                    <ul class="list-unstyled list-inline">
+                        <li class="list-inline-item">
+                            <a href="#" class="btn-floating text-muted" style="font-size: 23px !important"><i class='bx bxl-instagram' ></i></a>
+                            <a href="#" class="btn-floating text-muted" style="font-size: 23px !important"><i class='bx bxl-youtube' ></i></a>
+                            <a href="#" class="btn-floating text-muted" style="font-size: 23px !important"><i class='bx bxl-facebook-circle'></i></a>
+                            <a href="#" class="btn-floating text-muted" style="font-size: 23px !important"><i class='bx bxl-twitter' ></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
